b70c6ec16964746b9b62c2127b951ad248024213
eduard Alexi cardona Grajales <edwardcardona1972@gmail.com>
2025-03-24 17:51:20 +0100

    Initial Commit

---
+++ TMDBProjectTests/TMDBProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBProjectTests.swift
+//  TMDBProjectTests
+//
+//  Created by Eduard Alexis Cardona Grajales on 24/3/25.
+//
+
+import Testing
+@testable import TMDBProject
+
+struct TMDBProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
