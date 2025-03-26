c115a261cfb16e05c58ef2ae3423dc76c4c92aac
eduard Alexi cardona Grajales <edwardcardona1972@gmail.com>
2025-03-26 16:56:57 +0100

    Rellenar tabla

--- TMDBProject/View/eduard/listado/ListaViewController.swift
+++ TMDBProject/View/eduard/listado/ListaViewController.swift
@@ -9,16 +9,32 @@ import UIKit
 
 class ListaViewController: UIViewController {
     
+    @IBOutlet weak var tv: UITableView!
     var presenter: ListaPresenter? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
         let providerProtocol: PeliculasProviderProtocol = PeliculasProviderMock()
         presenter = ListaPresenter(peliculasProviderProtocol: providerProtocol)
+        
+        tv.delegate = self
+        tv.dataSource = self
+        //añadir los delegados de la tabla ejemplo tv.delegate = self
         Task{
             await presenter?.getPeliculas()
-            for peli in presenter?.peliculas ?? [] {
-                print(peli.title)    }
+            tv.reloadData()
         }
+    }
+}
+extension ListaViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return presenter?.peliculas.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let pelicula = presenter?.peliculas[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = pelicula?.title
+        return cell
     }
 }
