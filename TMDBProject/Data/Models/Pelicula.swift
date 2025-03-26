a169b54335bbbf1d6d52d6c734c6e9a8a6bd4036
eduard Alexi cardona Grajales <edwardcardona1972@gmail.com>
2025-03-26 11:32:00 +0100

    Feature/Estructuras

--- TMDBProject/Data/Models/Pelicula.swift
+++ TMDBProject/Data/Models/Pelicula.swift
@@ -5,7 +5,22 @@
 //  Created by Eduard Alexis Cardona Grajales on 25/3/25.
 //
 
-struct Pelicula: Codable {
+struct ResponseMasPopulares: Decodable {
+    let page: Int
+    let results: [Pelicula]
+}
+struct Pelicula: Decodable {
+    let adult: Bool
+    let ´genere_ids´:[Int]
     let id: Int
+    let original_language: String
+    let original_title: String
+    let overview: String
+    let popularity: Double
+    let poster_path: String?
+    let release_date: String
     let title: String
+    let video: Bool
+    let vote_average: Double
+    let vote_count: Int
 }
