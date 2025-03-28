12c3027ea4ec3d2b1797ed662b4b4120d0d81478
eduard Alexi cardona Grajales <edwardcardona1972@gmail.com>
2025-03-28 12:49:18 +0100

    combine con nuevo metodo

--- TMDBProject/View/eduard/listado/ListaViewModel.swift
+++ TMDBProject/View/eduard/listado/ListaViewModel.swift
@@ -4,21 +4,25 @@
 //
 //  Created by Eduard Alexis Cardona Grajales on 25/3/25.
 //
-
 import Foundation
 import Combine
 class ListaViewModel {
     
     var peliculasProviderProtocol: PeliculasProviderProtocol
     var peliculas: [Pelicula] = []
-    var reloadData = PassthroughSubject<Void, Error>()
+    var reloadData = PassthroughSubject<Void, Never>()
+    var anyPublisher: Set<AnyCancellable> = []
+    
     init(peliculasProviderProtocol: PeliculasProviderProtocol) {
         self.peliculasProviderProtocol = peliculasProviderProtocol
     }
     
     func getPeliculas(){
-        let listaPeliculas = peliculasProviderProtocol.getPeliculas()
-        peliculas = listaPeliculas.results
+        peliculasProviderProtocol.getPeliculas().sink { (completion) in
+            print("Completion: \(completion)")
+        } receiveValue: { (peliculas) in
+            self.peliculas = peliculas.results
+        }.store(in: &anyPublisher)
         reloadData.send(())
     }
 }
