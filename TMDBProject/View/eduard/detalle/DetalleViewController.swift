cd9de47ae3c792af6eedd27e2c51c9270306e6f2
eduard Alexi cardona Grajales <edwardcardona1972@gmail.com>
2025-03-28 10:26:36 +0100

    detalles de imagen

--- TMDBProject/View/eduard/detalle/DetalleViewController.swift
+++ TMDBProject/View/eduard/detalle/DetalleViewController.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class DetalleViewController: UIViewController {
     
+    @IBOutlet weak var viewPersonaje: UIImageView!
+    @IBOutlet weak var descripcion: UITextView!
     var pelicula: Pelicula?
-
+    var selectedTypeIndex: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //tv.delegate = self
+        //tv.dataSource = self
+        
     }
 }
+
--- TMDBProject/View/eduard/listado/ListaViewController.swift
+++ TMDBProject/View/eduard/listado/ListaViewController.swift
@@ -63,7 +63,4 @@ extension ListaViewController: UITableViewDelegate, UITableViewDataSource {
         }.resume()
         return cell
     }
-    
 }
-
-
